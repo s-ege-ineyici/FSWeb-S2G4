6fd8c781215905afcd635f8bbd1f100efc906531
s-ege-ineyici <ineyiciege@gmail.com>
2025-04-09 21:22:12 +0300

    çemberin çevresini bulma fonksiyonu yapıldı

--- index.js
+++ index.js
@@ -52,8 +52,9 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  let cevre = 2 * pi * yaricap
+  return cevre;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
